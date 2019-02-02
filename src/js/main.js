8d7586e3fa428a45baa11378ab8a0c0d4c113ebb
Christoph Franke <krito@krito.de>
2019-02-02 14:37:27 +0100

    show data from set1

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,25 @@
 import Highcharts from  'highcharts/highmaps'
 import map from '@highcharts/map-collection/custom/world.geo.json'
-import data from '../data/set1.json'
+import set1 from '../data/set1.json'
 // console.log(map.features.map(feature => feature.id))
+// console.log(data)
 
 window.addEventListener('load', () => {
-	const data = map.features.map(feature => ({
-		'hc-key': feature.id.toLowerCase(),
-		value: 10 * Math.random()
+	// const data = map.features.map(feature => ({
+	// 	'hc-key': feature.id.toLowerCase(),
+	// 	value: 10 * Math.random()
+	// }))
+
+	const data = set1.map(([key, value]) => ({
+		'hc-key': key.toLowerCase(),
+		value
 	}))
+
+	const max = data.map(point => point.value).reduce((max, value) => Math.max(max, value), 0)
 
 	Highcharts.mapChart('container', {
     series: [{
-      data: data,
+      data,
       mapData: map,
       joinBy: 'hc-key',
       allAreas: false,
@@ -36,7 +44,7 @@ window.addEventListener('load', () => {
 
     colorAxis: {
       min: 0,
-      max: 10
+      max
     },
 	})
 })
